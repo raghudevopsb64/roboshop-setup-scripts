5b02f21f4554b975d0ce265ddbe7d636f8c642e8
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 08:14:50 +0530

    Update roboshop-setup-scripts/

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+USER_ID=$(id -u)
+
+if [ "$USER_ID" -ne 0 ]; then
+  echo You are suppose to be running this script as sudo
+else
+
 yum install nginx -y
 systemctl enable nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
@@ -11,3 +17,4 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
+fi
