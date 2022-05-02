2bd561f36228b7735159cc4c31152f85f7b516ae
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 08:51:17 +0530

    Update roboshop-setup-scripts/

--- components/frontend.sh
+++ components/frontend.sh
@@ -4,15 +4,16 @@ source components/common.sh
 checkRootUser
 
 
-yum install nginx -y
-systemctl enable nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+yum install nginx -y >/tmp/roboshop.log
+systemctl enable nginx >/tmp/roboshop.log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
+cd /usr/share/nginx/html >/tmp/roboshop.log
+rm -rf * >/tmp/roboshop.log
+unzip /tmp/frontend.zip >/tmp/roboshop.log
+mv frontend-main/* . >/tmp/roboshop.log
+mv static/* . >/tmp/roboshop.log
+rm -rf frontend-main README.md >/tmp/roboshop.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >/tmp/roboshop.log
+systemctl restart nginx >/tmp/roboshop.log
+
 
