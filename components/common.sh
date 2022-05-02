e30f3edc5aeaae12366be9fa8a5322fcc255a510
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 08:32:17 +0530

    Update roboshop-setup-scripts/

--- components/common.sh
+++ components/common.sh
@@ -2,7 +2,7 @@ checkRootUser() {
   USER_ID=$(id -u)
 
   if [ "$USER_ID" -ne "0" ]; then
-    echo You are suppose to be running this script as sudo or root user
+    echo -e "\e[31mYou are suppose to be running this script as sudo or root user\e[0m"
     exit
   fi
 }
