4214e8e53417d472ca04f12cacc4de250dea875e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-03 07:49:15 +0530

    Update roboshop-setup-scripts/

--- components/frontend.sh
+++ components/frontend.sh
@@ -5,35 +5,35 @@ checkRootUser
 
 
 ECHO "Installing Nginx"
-yumm install nginx -y >>${LOG_FILE}
+yumm install nginx -y &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Downloading Frontend Code"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>${LOG_FILE}
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
 statusCheck $?
 
 cd /usr/share/nginx/html
 
 ECHO "Removing Old Files"
-rm -rf * >>${LOG_FILE}
+rm -rf * &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Extracting Zip Content"
-unzip /tmp/frontend.zip >>${LOG_FILE}
+unzip /tmp/frontend.zip &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Copying extracted Content"
-mv frontend-main/* . >>${LOG_FILE}
-mv static/* . >>${LOG_FILE}
-rm -rf frontend-main README.md >>${LOG_FILE}
+mv frontend-main/* . &>>${LOG_FILE}
+mv static/* . &>>${LOG_FILE}
+rm -rf frontend-main README.md &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Copy RoboShop Nginx Config"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >>${LOG_FILE}
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Start Nginx Service"
-systemctl enable nginx >>${LOG_FILE}
-systemctl restart nginx >>${LOG_FILE}
+systemctl enable nginx &>>${LOG_FILE}
+systemctl restart nginx &>>${LOG_FILE}
 statusCheck $?
 
