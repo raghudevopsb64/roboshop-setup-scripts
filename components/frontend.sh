b2665fae9c3f1c3a884634b6152d92944c9a1b79
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-03 07:45:55 +0530

    Update roboshop-setup-scripts/

--- components/frontend.sh
+++ components/frontend.sh
@@ -8,31 +8,31 @@ ECHO "Installing Nginx"
 yum install nginx -y >>${LOG_FILE}
 statusCheck $?
 
-echo "Downloading Frontend Code"
+ECHO "Downloading Frontend Code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>${LOG_FILE}
 statusCheck $?
 
 cd /usr/share/nginx/html
 
-echo "Removing Old Files"
+ECHO "Removing Old Files"
 rm -rf * >>${LOG_FILE}
 statusCheck $?
 
-echo "Extracting Zip Content"
+ECHO "Extracting Zip Content"
 unzip /tmp/frontend.zip >>${LOG_FILE}
 statusCheck $?
 
-echo "Copying extracted Content"
+ECHO "Copying extracted Content"
 mv frontend-main/* . >>${LOG_FILE}
 mv static/* . >>${LOG_FILE}
 rm -rf frontend-main README.md >>${LOG_FILE}
 statusCheck $?
 
-echo "Copy RoboShop Nginx Config"
+ECHO "Copy RoboShop Nginx Config"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf >>${LOG_FILE}
 statusCheck $?
 
-echo "Start Nginx Service"
+ECHO "Start Nginx Service"
 systemctl enable nginx >>${LOG_FILE}
 systemctl restart nginx >>${LOG_FILE}
 statusCheck $?
