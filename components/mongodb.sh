a5185d9501b296fd7b47e383994ba1d7ce10f554
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-04 08:23:31 +0530

    Update roboshop-setup-scripts/

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -29,3 +29,7 @@ ECHO "Installing MongoDB"
 yum install -y mongodb-org  &>>${LOG_FILE}
 statusCheck $?
 
+ECHO "Configure Listen Address in MonogBD Configuration"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+statusCheck $?
+
