287d1d6135c8342133f76b271a6ba082c533d1c9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-05 08:39:35 +0530

    Update roboshop-setup-scripts/

---
+++ components/mysql.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source components/common.sh
+checkRootUser
+
+# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+# yum install mysql-community-server -y
+# systemctl enable mysqld
+# systemctl start mysqld
+# grep temp /var/log/mysqld.log
+
+# mysql_secure_installation
+# mysql -uroot -pRoboShop@1
+#uninstall plugin validate_password;
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -u root -pRoboShop@1 <shipping.sql
+
+ECHO "Setup MySQL Yum repo"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Install MySQL Server"
+yum install mysql-community-server -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Start MySQL Service"
+systemctl enable mysqld  &>>${LOG_FILE} && systemctl start mysqld  &>>${LOG_FILE}
+statusCheck $?
+
