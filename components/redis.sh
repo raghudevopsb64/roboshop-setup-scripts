5892f24267b0da86ce56d4f0bdb72683bbd7aca0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-05 07:57:08 +0530

    Update roboshop-setup-scripts/

--- components/redis.sh
+++ components/redis.sh
@@ -3,14 +3,6 @@
 source components/common.sh
 checkRootUser
 
-# curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-# yum install redis -y
-
-#pdate the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
-# systemctl enable redis
-# systemctl start redis
-
-
 ECHO "Configure YUM Repos"
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
 statusCheck $?
