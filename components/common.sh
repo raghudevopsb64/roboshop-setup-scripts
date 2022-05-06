1d38b4a42848510c13fa0513ce2d578ae0e52355
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-06 07:54:27 +0530

    Update roboshop-setup-scripts/

--- components/common.sh
+++ components/common.sh
@@ -84,5 +84,5 @@ JAVA() {
   cd /home/roboshop/${COMPONENT} && mvn clean package &>>${LOG_FILE} && mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG_FILE}
   statusCheck $?
 
-  #SYSTEMD_SETUP
+  SYSTEMD_SETUP
 }
