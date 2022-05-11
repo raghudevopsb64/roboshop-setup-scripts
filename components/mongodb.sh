c60b514a67f8856926f4c01c0f4681d7857d9781
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-11 07:48:11 +0530

    Update roboshop-setup-scripts/

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -3,16 +3,6 @@
 source components/common.sh
 checkRootUser
 
-
-## curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-#
-## cd /tmp
-## unzip mongodb.zip
-## cd mongodb-main
-## mongo < catalogue.js
-## mongo < users.js
-
-
 ECHO "Setup MongoDB Yum repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
 statusCheck $?
